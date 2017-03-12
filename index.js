790d45f81577680729958bafd4c1e348da5b30ab
王麟 <wanglin@ezbuy.com>
2017-03-13 00:07:25 +0800

    Update To langs-loader

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 var fs = require("fs");
 var path = require("path");
 var qs = require("querystring");
+var loaderUtils = require('loader-utils');
+var po2json = require("po2json");
 
+var getFileCode = (filename) => {
+	const nameArr = filename.split(".");
+	return nameArr[nameArr.length - 2];
+}
 
 module.exports = function(source) {
 	this.cacheable && this.cacheable();
 	var resourcePath = this.resourcePath;
-	var query = qs.parse(this.query.slice(1));
-	if (typeof query.code === "undefined" ) {
-		return source;
-	}
-	var suffix = query.code;
-	var callback = this.async();
-	var extname = path.extname(resourcePath);
-	var newResourcePath = resourcePath.replace(extname,"."+suffix+extname);
+	var options = loaderUtils.parseQuery(this.query);
 
-	if(!callback) {
-		return fs.readFileSync(newResourcePath);
+	if(typeof options.code !== "undefined" || (typeof options.isLoadAll !== "undefined" && options.isLoadAll)){
+		var resourceDir = path.dirname(resourcePath);
+
+		if(typeof options.code !== "undefined"){
+			var poPathname = path.join(resourceDir,path.basename(resourcePath,path.extname(resourcePath)))+"."+options.code+".po";
+			return po2json.parse(fs.readFileSync(poPathname),options);
+		}
+
+		if (typeof options.isLoadAll !== "undefined" && options.isLoadAll) {
+			var jsonData = {allResources: true};
+			files = fs.readdirSync(resourceDir).filter((path)=>path.endsWith(".po"));
+
+			for(var i=0;i<files.length;i++){
+				var fileName = files[i];
+				var fileCode = getFileCode(files[i]);
+				if(typeof fileCode !== "undefined"){
+					var filePath = path.join(resourceDir,fileName);
+					jsonData[fileCode] = po2json.parse(fs.readFileSync(filePath),options);
+				}
+			}
+			return jsonData;
+		}
 	}
 
-	fs.readFile(newResourcePath,function(error, content){
-		if(error) return callback(error);
-		callback(null,content);
-	});
+	return po2json.parse(source, options);
 }
